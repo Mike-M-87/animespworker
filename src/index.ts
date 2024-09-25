25d41a40663f3d1b24c20e9aceaf6bf1e4620e5b
Mike-M-87 <micminn87@gmail.com>
2024-09-26 00:27:47 +0300

    change theme color

--- src/index.ts
+++ src/index.ts
@@ -200,78 +200,7 @@ interface TelbotResp {
   description: string;
 }
 
-
-function renderMessagePage(isError: boolean, messageContent: string): string {
-  return `
-<!DOCTYPE html>
-<html>
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-
-  <title>Jin Woo Worker</title>
-  <meta property="og:title" content="Jin Woo Worker" />
-
-  <meta name="description" content="Notifications update form for Jin Woo Animes telegram channel">
-  <meta property="og:description" content="Notifications update form for Jin Woo Animes telegram channel" />
-
-  <meta property="og:url" content="https://animespworker.micminn872837.workers.dev" />
-
-  <meta property="og:image" content="https://i.ibb.co/DrFbTBK/sl2-1.jpg" />
-  <meta property="og:image:type" content="image/jpeg" />
-  <meta property="og:image:width" content="500" />
-  <meta property="og:image:height" content="500" />
-
-  <meta name="theme-color" content="#324784" />
-  <meta name="color-scheme" content="light dark" />
-
-  <link rel="apple-touch-icon" href="https://i.ibb.co/VSnjv9x/sl2-1.png" />
-  <link rel="shortcut icon" type="image/png" href="https://i.ibb.co/VSnjv9x/sl2-1.png" />
-  <meta name="apple-mobile-web-app-capable" content="yes" />
-  <meta name="apple-mobile-web-app-title" content="Jin Woo Worker" />
-  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-
-  <meta name="display" content="standalone">
-  <link rel="manifest" crossorigin="use-credentials"
-    href="https://gist.githubusercontent.com/Mike-M-87/c4bb8d50dd4c5181aebd69978d594fb4/raw/4fe12844f9f252e14a7eebe159bdf9222c45c9c7/manifest1.json" />
-
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<style>
-  .maincontainer {
-    background-image: url("https://r4.wallpaperflare.com/wallpaper/683/96/3/solo-leveling-sung-jin-woo-manga-anime-boys-hd-wallpaper-333b13cd2dc9fbf52f06e2e7d83be858.jpg");
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-</style>
-
-<body>
-  <div class="maincontainer text-white flex h-svh w-svw bg-black flex-col justify-center items-center">
-    <div
-      class="w-full max-w-3xl p-10 bg-black bg-opacity-60 backdrop-blur-lg border dark:border-b-white/50 dark:border-t-white/50 border-b-white/20 sm:border-t-white/20 shadow-[20px_0_20px_20px] shadow-slate-500/10 dark:shadow-white/20 rounded-lg border-white/20 border-l-white/20 border-r-white/20 sm:shadow-sm lg:rounded-xl lg:shadow-none">
-
-      <div class="flex flex-col">
-        <h3 class="text-xl font-semibold leading-6 tracking-tighter ${isError ? "text-red-500" : "text-green-500"}">
-          ${messageContent}
-        </h3>
-      </div>
-
-      <a href="https://animespworker.micminn872837.workers.dev"
-        class="w-fit mx-auto mt-6 p-4 hover:bg-black hover:text-white hover:ring hover:ring-white transition duration-300 flex items-center justify-center rounded-md text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black">
-        Go Back
-      </a>
-    </div>
-  </div>
-</body>
-</html>`;
-}
-
-
-function formPage(): string {
-  return `
+const templatePage = `
 <!DOCTYPE html>
 <html>
 
@@ -316,7 +245,36 @@ function formPage(): string {
     background-size: cover;
   }
 </style>
+`
 
+function renderMessagePage(isError: boolean, messageContent: string): string {
+  return `
+  ${templatePage}
+<body>
+  <div class="maincontainer px-3 py-10 text-white flex h-svh w-svw bg-black flex-col justify-center items-center">
+    <div
+      class="w-full max-w-3xl py-10 px-6 sm:px-10 bg-black bg-opacity-60 backdrop-blur-lg border dark:border-b-white/50 dark:border-t-white/50 border-b-white/20 sm:border-t-white/20 shadow-[20px_0_20px_20px] shadow-slate-500/10 dark:shadow-white/20 rounded-lg border-white/20 border-l-white/20 border-r-white/20 sm:shadow-sm lg:rounded-xl lg:shadow-none">
+
+      <div class="flex flex-col">
+        <h3 class="text-xl font-semibold leading-6 tracking-tighter ${isError ? "text-red-500" : "text-green-500"}">
+          ${messageContent}
+        </h3>
+      </div>
+
+      <a href="https://animespworker.micminn872837.workers.dev"
+        class="w-fit mx-auto mt-6 p-4 hover:bg-black hover:text-white hover:ring hover:ring-white transition duration-300 flex items-center justify-center rounded-md text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black">
+        Go Back
+      </a>
+    </div>
+  </div>
+</body>
+</html>`;
+}
+
+
+function formPage(): string {
+  return `
+  ${templatePage}
 <body>
   <div
     class="maincontainer px-3 py-10 text-white flex min-h-svh h-full w-svw bg-black flex-col justify-center items-center">
@@ -460,6 +418,5 @@ function formPage(): string {
 
   </div>
 </body>
-
 </html>`;
 }
